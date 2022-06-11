9b1832149f86fbc332c0a2280112498a0acb30f9
Programmercito <osboceo@live.com>
2022-06-10 23:59:45 -0400

    primera version

---
+++ src/test/java/org/prueba/ejercicios/uno/Ejercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prueba.ejercicios.uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
