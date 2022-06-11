392f66ba97a1a503a505dd994c7ebe3e152d5e03
Programmercito <osboceo@live.com>
2022-06-11 12:56:39 -0400

    ejercicio numero 1 terminado con el cambio

---
+++ src/main/java/org/prueba/ejercicios/uno/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.prueba.ejercicios.uno.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ *
+ * @author Programmercito <devtecpro.org>
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic();
+        return http.build();
+    }
+
+
+}
