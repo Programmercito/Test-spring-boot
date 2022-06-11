0b27a98adc5a8f4b480ede4a2e5697c46e2a3940
Programmercito <osboceo@live.com>
2022-06-11 01:28:44 -0400

    base lista y entity

---
+++ src/main/java/org/prueba/ejercicios/uno/model/entities/User.java
@@ -0,0 +1,102 @@
+package org.prueba.ejercicios.uno.model.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Programmercito <devtecpro.org>
+ */
+@Entity
+@Table(name = "userapp")
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "pk_user", sequenceName = "seg_user", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "pk_user")
+    @Column(name = "id", nullable = true, updatable = false, unique = true)
+    private long id;
+    @Column(name = "firstname")
+    private String firstname;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "username")
+    private String username;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
