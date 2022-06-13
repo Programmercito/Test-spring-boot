6baea1766d99fdbadff226f9ae0e1805de1abe3a
Programmercito <osboceo@live.com>
2022-06-13 15:13:41 -0400

    agregando test simple

--- src/test/java/org/prueba/ejercicios/uno/Ejercicio1ApplicationTests.java
+++ src/test/java/org/prueba/ejercicios/uno/Ejercicio1ApplicationTests.java
@@ -1,13 +1,40 @@
 package org.prueba.ejercicios.uno;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.prueba.ejercicios.uno.model.entities.User;
+import org.prueba.ejercicios.uno.model.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Ejercicio1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    UserService service;
+    User usuario;
+
+    @Test
+    void test() {
+        User usuario = new User();
+        usuario.setFirstname("Joaquin");
+        usuario.setLastname("Heredia");
+        usuario.setEmail("joaquin@server.com");
+        usuario.setUsername("jheredia");
+        User resul = service.save(usuario);
+        System.out.println("test insert");
+        assertNotNull(resul);
+        this.usuario=resul;
+        usuario.setFirstname("Joaquin Alberto");
+        resul = service.save(usuario);
+        System.out.println("test update");
+        assertNotNull(resul);
+        this.usuario=resul;
+        resul = service.find(usuario.getId());
+        System.out.println("test get");
+        assertNotNull(resul);
+        service.delete(usuario);
+        System.out.println("test delete");
+    }
 
 }
